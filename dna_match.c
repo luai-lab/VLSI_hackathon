8e25e91a8fbd6fc7a7b72fe0a7c46b6c9949e519
Luai <luai.inbox@gmail.com>
2026-06-21 21:08:58 +0300

    Implement hardware acceleration for Smith-Waterman (16k)
    Added hardware acceleration for Smith-Waterman algorithm and improved cycle counting.

--- dna_match.c
+++ dna_match.c
@@ -17,37 +17,63 @@
 #define GAP_EXT    -1
 
 #define MAX_LEN   32
-
 #define NEG_INF  (-1000000)
 
 #define NUM_OF_REFS 8
+#define HW_TIMEOUT_POLLS 10000u
 
-static int M[MAX_LEN][MAX_LEN];
-static int I[MAX_LEN][MAX_LEN];
-static int D[MAX_LEN][MAX_LEN];
+// Smith-Waterman Accelerator register map.
+// Hackathon accelerator base address = 0x80001300
+#define ACCEL_SW_CONTROL          0x80001300
+#define ACCEL_SW_LENGTHS          0x80001304
+#define ACCEL_SW_QUERY_REG_BASE   0x80001308
+#define ACCEL_SW_REF_REG_BASE     0x80001328
+#define ACCEL_SW_RESULT           0x80001348
+
+#define SW_CTRL_GO_BIT            0x00000001u
+#define SW_CTRL_DONE_BIT          0x80000000u
+
+#define READ_GPIO(addr)         (*(volatile unsigned int *)(addr))
+#define WRITE_GPIO(addr, value) { (*(volatile unsigned int *)(addr)) = (value); }
+
+static inline unsigned int pack_chars(char c0, char c1, char c2, char c3)
+{
+   return  ((unsigned int)(c0 & 0xFF))
+        | (((unsigned int)(c1 & 0xFF)) <<  8)
+        | (((unsigned int)(c2 & 0xFF)) << 16)
+        | (((unsigned int)(c3 & 0xFF)) << 24);
+}
+
+static void write_hw_sequence(unsigned int base_addr, const char *seq, int len)
+{
+   for (int i = 0; i < 8; i++) {
+      int idx = i * 4;
+
+      char c0 = (idx < len)     ? seq[idx]     : 0;
+      char c1 = (idx + 1 < len) ? seq[idx + 1] : 0;
+      char c2 = (idx + 2 < len) ? seq[idx + 2] : 0;
+      char c3 = (idx + 3 < len) ? seq[idx + 3] : 0;
+
+      WRITE_GPIO(base_addr + (i * 4), pack_chars(c0, c1, c2, c3));
+   }
+}
+
+static int M[MAX_LEN + 1][MAX_LEN + 1];
+static int I[MAX_LEN + 1][MAX_LEN + 1];
+static int D[MAX_LEN + 1][MAX_LEN + 1];
 
 static inline int max2(int a, int b)
 {
     return (a > b) ? a : b;
 }
 
-static inline int max3(int a, int b, int c)
+static inline int max4(int a, int b, int c, int d)
 {
     int m = a;
 
-    if(b > m) m = b;
-    if(c > m) m = c;
-
-    return m;
-}
-
-static inline int max4(int a,int b,int c,int d)
-{
-    int m = a;
-
-    if(b > m) m = b;
-    if(c > m) m = c;
-    if(d > m) m = d;
+    if (b > m) m = b;
+    if (c > m) m = c;
+    if (d > m) m = d;
 
     return m;
 }
@@ -116,14 +142,65 @@ int smith_waterman_affine(
     return best_score;
 }
 
+static int smith_waterman_hw_prepare_query(const char *query)
+{
+   int q_len = strlen(query);
+
+   if (q_len > MAX_LEN) {
+      return NEG_INF;
+   }
+
+   write_hw_sequence(ACCEL_SW_QUERY_REG_BASE, query, q_len);
+   return q_len;
+}
+
+int smith_waterman_hw_run_ref(const char *ref, int q_len)
+{
+   int r_len = strlen(ref);
+   unsigned int polls = 0;
+
+   if ((q_len > MAX_LEN) || (r_len > MAX_LEN)) {
+      return NEG_INF;
+   }
+
+   unsigned int packed_lens = (q_len & 0xFF) | ((r_len & 0xFF) << 8);
+
+   WRITE_GPIO(ACCEL_SW_LENGTHS, packed_lens);
+   write_hw_sequence(ACCEL_SW_REF_REG_BASE, ref, r_len);
+   WRITE_GPIO(ACCEL_SW_CONTROL, SW_CTRL_GO_BIT);
+
+   while ((READ_GPIO(ACCEL_SW_CONTROL) & SW_CTRL_DONE_BIT) == 0) {
+      polls++;
+
+      if (polls >= HW_TIMEOUT_POLLS) {
+         return NEG_INF;
+      }
+   }
+
+   return (int)READ_GPIO(ACCEL_SW_RESULT);
+}
+
+int smith_waterman_hw(const char *ref, const char *query)
+{
+   int q_len = smith_waterman_hw_prepare_query(query);
+
+   if (q_len < 0) {
+      return NEG_INF;
+   }
+
+   return smith_waterman_hw_run_ref(ref, q_len);
+}
+
+static unsigned int read_cycle_counter(void)
+{
+    return (unsigned int)pspMachinePerfCounterGet(D_PSP_COUNTER0);
+}
+
 int main(void)
 {
-    const char *query =
-        "ACGTCGTACGTACGTA";
+    const char *query = "ACGTCGTACGTACGTA";
 
-    int score[NUM_OF_REFS]; 
-
-    const char *references[] =
+    const char *references[NUM_OF_REFS] =
     {
         "ACGTACGTACGTACGT",
         "ACGTTCGTACGTACGT",
@@ -135,36 +212,78 @@ int main(void)
         "ACGTACGTGCGTACGT"
     };
 
+    int sw_score[NUM_OF_REFS];
+    int hw_score[NUM_OF_REFS];
 
-    // read time here
-   int cyc_beg, cyc_end;
+    unsigned int cyc_beg;
+    unsigned int cyc_end;
+    unsigned int sw_cycles;
+    unsigned int hw_cycles;
 
-   pspMachinePerfMonitorEnableAll();
-   pspMachinePerfCounterSet(D_PSP_COUNTER0, D_CYCLES_CLOCKS_ACTIVE);
-   cyc_beg = pspMachinePerfCounterGet(D_PSP_COUNTER0);
+    int q_len;
 
+    pspMachinePerfMonitorEnableAll();
+    pspMachinePerfCounterSet(D_PSP_COUNTER0, D_CYCLES_CLOCKS_ACTIVE);
 
-    for(int i=0;i<NUM_OF_REFS;i++)
-    {
-        score[i] =
-            smith_waterman_affine(
-                references[i],
-                query);
+    cyc_beg = read_cycle_counter();
 
+    for (int i = 0; i < NUM_OF_REFS; i++) {
+        sw_score[i] = smith_waterman_affine(references[i], query);
     }
 
-   
-    // read timer here and print execution time
-   cyc_end = pspMachinePerfCounterGet(D_PSP_COUNTER0);
+    cyc_end = read_cycle_counter();
+    sw_cycles = cyc_end - cyc_beg;
 
-   printf("Cycles = %d\n", cyc_end-cyc_beg);
+    q_len = smith_waterman_hw_prepare_query(query);
 
-    for(int i=0;i<NUM_OF_REFS;i++)
-    {
-        printf(
-            "Reference %d score = %d\n",
-            i,
-            score[i]);
+    if (q_len < 0) {
+        for (int i = 0; i < NUM_OF_REFS; i++) {
+            hw_score[i] = NEG_INF;
+        }
+
+        hw_cycles = 0;
+    }
+    else {
+        cyc_beg = read_cycle_counter();
+
+        for (int i = 0; i < NUM_OF_REFS; i++) {
+            hw_score[i] = smith_waterman_hw_run_ref(references[i], q_len);
+
+            if (hw_score[i] == NEG_INF) {
+                for (int j = i + 1; j < NUM_OF_REFS; j++) {
+                    hw_score[j] = NEG_INF;
+                }
+
+                break;
+            }
+        }
+
+        cyc_end = read_cycle_counter();
+        hw_cycles = cyc_end - cyc_beg;
+    }
+
+    printf("Smith-Waterman comparison\n");
+    printf("Software cycles = %u\n", sw_cycles);
+    printf("Hardware cycles = %u\n", hw_cycles);
+
+    if (hw_cycles != 0 && hw_score[0] == NEG_INF) {
+        printf("Hardware timeout: check bitstream/register map\n");
+    }
+
+    if (hw_cycles != 0 && hw_score[0] != NEG_INF) {
+        printf("Speedup x100 = %u\n", (sw_cycles * 100u) / hw_cycles);
+    }
+
+    printf("\n");
+    printf("Ref | SW score | HW score | Match\n");
+    printf("----------------------------------\n");
+
+    for (int i = 0; i < NUM_OF_REFS; i++) {
+        printf("%3d | %8d | %8d | %s\n",
+               i,
+               sw_score[i],
+               hw_score[i],
+               (sw_score[i] == hw_score[i]) ? "YES" : "NO");
     }
 
     return 0;
